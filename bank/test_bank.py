a10a6360cf87d81a886631187a141c4c0ea23571
Shashank <spandha780@gmail.com>
2024-09-09 00:18:13 +0530

    Updated test script

--- bank/test_bank.py
+++ bank/test_bank.py
@@ -10,11 +10,7 @@ def test_case_insensitivity():
 	assert value("HELLO") == 0
 	assert value("hEllo") == 0
 
-def test_allowing_entire_phrase():
-	assert value("hello world") == 0
-	assert value("HEY, HOW ARE YOU?") == 20
-
 def test_partial():
 	assert value("hey") == 20
-	assert value("hola amigo") == 20
+	assert value("Hola") == 20
 
